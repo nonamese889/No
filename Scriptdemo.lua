8484f494fc5fff585eae71e7c0ae0ab9dc72a2d9
nonamese889 <baconacquy@gmail.com>
2025-02-15 21:03:34 +0700

    Add files via upload

--- Scriptdemo.lua
+++ Scriptdemo.lua
@@ -1,4 +1,4 @@
-loadstring(game:HttpGet(("https://raw.githubusercontent.com/baoroblox/tong-hop-V1/refs/heads/main/baotonghopV1.lua")))()
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/daucobonhi/Ui-Redz-V2/refs/heads/main/UiREDzV2.lua")))()
 
        local Window = MakeWindow({
          Hub = {
